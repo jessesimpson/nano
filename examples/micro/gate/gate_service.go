d3abac6fb6655ef9c7819632fa4445ab026406c2
yxares <yxares@gmail.com>
2026-01-04 23:00:05 +0800

    gate不处理HelleRequest

--- examples/micro/gate/gate_service.go
+++ examples/micro/gate/gate_service.go
@@ -1,8 +1,6 @@
 package gate
 
 import (
-	"log"
-
 	"github.com/lonng/nano/component"
 	"github.com/lonng/nano/session"
 )
@@ -11,18 +9,12 @@ type GateService struct{ component.Base }
 
 func newGateService() *GateService { return &GateService{} }
 
-type HelloRequest struct {
+type EchoRequest struct {
 	Name string `json:"name"`
 }
 
 // Hello forwards the request to GameService.Hello on game nodes.
-func (gs *GateService) Hello(s *session.Session, msg *HelloRequest) error {
+func (gs *GateService) Hello(s *session.Session, msg *EchoRequest) error {
 	// log receipt and forward to GameService; response will be routed back to client
-	log.Printf("GateService.Hello received: session=%d name=%s", s.ID(), msg.Name)
-	if err := s.RPC("GameService.Hello", msg); err != nil {
-		log.Printf("GateService.Hello RPC error: %v", err)
-		return err
-	}
-	log.Printf("GateService.Hello forwarded to GameService for session=%d", s.ID())
 	return nil
 }
